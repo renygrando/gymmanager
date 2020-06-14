b62805b6e185d46143ea7d1cde1d370abc1571e5
Reny Grando <renygrando@gmail.com>
2020-06-14 19:17:08 -0300

    separando funcao da rota instructor

---
+++ instructors.js
@@ -0,0 +1,21 @@
+const fs = require('fs')
+const data = require('./data.json')
+
+exports.post = (req, res) => {
+    
+    const keys = Object.keys(req.body)
+
+    for(key of keys) {
+        if (req.body[key] == ""){
+            return res.send("Por favor, preencha todos os campos!")
+        }
+    }
+    data.instructors.push(req.body)
+
+    fs.writeFile("data.json", JSON.stringify(data,null, 2), function(err){
+        if (err) return res.send("ERRO!")
+        
+    })
+    return res.redirect("/instructors")
+    // return res.send(req.body)
+}
