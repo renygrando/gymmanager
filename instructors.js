cfcaee1a9625eb3f2a9e53ed383f4dd0f1de5ebc
Reny Grando <renygrando@gmail.com>
2020-06-16 21:44:33 -0300

    separando funcao da rota instructor

--- instructors.js
+++ instructors.js
@@ -10,12 +10,27 @@ exports.post = (req, res) => {
             return res.send("Por favor, preencha todos os campos!")
         }
     }
-    data.instructors.push(req.body)
+
+    let { avatar_url, birth, gender, services, name } = req.body
+    
+    birth = Date.parse(birth)
+    const created_at = Date.now()
+    const id = Number(data.instructors.length + 1)
+
+    data.instructors.push({
+        id,
+        avatar_url,
+        name,
+        birth, 
+        gender,
+        services,
+        created_at
+    })
 
     fs.writeFile("data.json", JSON.stringify(data,null, 2), function(err){
+
         if (err) return res.send("ERRO!")
-        
+
+        return res.redirect("/instructors")
     })
-    return res.redirect("/instructors")
-    // return res.send(req.body)
 }
