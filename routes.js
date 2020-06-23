53c69bd1a583875ec7227ea36810afe2bbf2d800
Reny Grando <renygrando@gmail.com>
2020-06-22 22:40:48 -0300

    add rota para enviar e editar intrutor

--- routes.js
+++ routes.js
@@ -15,6 +15,10 @@ routes.get('/instructors/create', (req, res) => {
 
     return res.render("instructors/create")
 })
+routes.get('/instructors/:id', instructors.show), (req, res) => {
+}
+routes.get('/instructors/:id/edit', instructors.edit)
+
 routes.post('/instructors', instructors.post )
 
 routes.get('/members', (req, res) => {
