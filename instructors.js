6060c8d1c3fda9b73162696b409730938e27e2c2
Reny Grando <renygrando@gmail.com>
2020-07-01 23:02:44 -0300

    adicionando função DELETE

--- instructors.js
+++ instructors.js
@@ -99,3 +99,20 @@ exports.put = (req, res) => {
         return res.redirect(`/instructors/${id}`)
     })
 }
+
+exports.delete = (req, res) => {
+    const { id } = req.body
+
+    const filteredInstructors = data.instructors.filter((instructor) => {
+        return instructor.id != id
+    })
+    
+    data.instructors = filteredInstructors
+    
+    fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err) {
+        if(err) return res.send("Write erros")
+
+        return res.redirect("/instructors")
+
+    })
+}
