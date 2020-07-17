ab99d1799ff64991dcd06e860674338abf53306e
Reny Grando <renygrando@gmail.com>
2020-07-16 23:36:06 -0300

    criando script do botao ativo

--- public/scripts.js
+++ public/scripts.js
@@ -1,25 +1,8 @@
-//CRIANDO ROTAS PARA CADA RECEITA
-const cards = document.querySelectorAll('.quick_recipes');
+const currentPage = location.pathname
+const menuItems = document.querySelectorAll("header .links a")
 
-for (let card of cards) {
-    card.addEventListener('click', function(){
-        const recipeId = card.getAttribute("id");
-        window.location.href = `/recipe-detail/${recipeId}`
-    })
-}
-
-// CRIANDO BOTÃO MOSTRA-ESCONDE NA PÁGINA DE RECEITAS
-const ingredient = document.querySelectorAll('.recipe_showhide');
-const showHide= document.querySelectorAll('.showhide_btn');
-
-for(let i = 0; i < showHide.length; i++) {
-    showHide[i].addEventListener('click', function() {
-        if (showHide[i].innerHTML == 'Esconder') {
-            ingredient[i].classList.add('active')
-            showHide[i].innerHTML = 'Mostrar'
-        } else {
-            ingredient[i].classList.remove('active')
-            showHide[i].innerHTML = 'Esconder'
-        }
-    })
+for (item of menuItems) {
+    if (currentPage.includes(item.getAttribute("href"))) {
+        item.classList.add("active")
+    }
 }
