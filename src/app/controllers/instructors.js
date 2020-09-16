bd06f916cc1484e378aa6ac0d9eb079128dfdd94
Reny Grando <renygrando@gmail.com>
2020-09-15 23:34:22 -0300

    refatorando module exports e pegando dados do model

--- src/app/controllers/instructors.js
+++ src/app/controllers/instructors.js
@@ -1,12 +1,17 @@
 const Intl = require('intl')
-const {
-    age,
-    date
-} = require("../../lib/utils")
+const Instructor = require("../models/instructor")
+const { age, date } = require("../../lib/utils")
 
 module.exports = {
     index(req, res) {
-        return res.render("instructors/index")
+        Instructor.all(function (instructors) {
+
+            return res.render("instructors/index", {
+                instructors
+            })
+
+        })
+
     },
     create(req, res) {
         return res.render("instructors/create")
@@ -19,19 +24,20 @@ module.exports = {
                 return res.send("Por favor, preencha todos os campos!")
             }
         }
-
-        let {
-            avatar_url,
-            birth,
-            gender,
-            services,
-            name
-        } = req.body
-
-        return
+        Instructor.create(req.body, function(instructor) {
+            return res.redirect(`/instructors/${instructor.id}`)
+        })
     },
     show(req, res) {
-        return
+        Instructor.find(req.params.id, function(instructor) {
+            if (!instructor) return res.send("Instructor not found!")
+
+            instructor.age = age(instructor.birth)
+            instructor.services = instructor.services.split(",")
+            instructor.created_at = date(instructor.created_at).format
+
+            return res.render("instructors/show", { instructor })
+        })
     },
     edit(req, res) {
         return
